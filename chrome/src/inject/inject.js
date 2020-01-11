a4d67c006a686dfc28138e1fdadd9cb361d5c2c4
lazynessmind <lazynessmind@gmail.com>
2020-01-11 10:09:29 +0000

    removed innerhtml

--- chrome/src/inject/inject.js
+++ chrome/src/inject/inject.js
@@ -21,7 +21,7 @@ chrome.extension.sendMessage({}, function (response) {
 
 			var style = document.createElement('style');
 			style.type = 'text/css';
-			style.innerHTML = `#vbplayer_${currId} {display: none !important;} #vplayer_${currId} {display: block !important;}`;
+			style.textContent = `#vbplayer_${currId} {display: none !important;} #vplayer_${currId} {display: block !important;}`;
 			(document.head || document.documentElement).appendChild(style);
 
 			console.log(`[tuga.tv_unblock] Injected style instructions;`)
